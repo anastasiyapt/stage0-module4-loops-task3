c9f0b8a28c2754d4b9ea8a84620c835f0ac93a2c
Anastasiya P <anastasiapatsei@gmail.com>
2023-02-14 17:49:43 +0200

    partially solved

--- src/main/java/school/mjc/stage0/loops/task3/AlphabetPrinter.java
+++ src/main/java/school/mjc/stage0/loops/task3/AlphabetPrinter.java
@@ -2,5 +2,10 @@ package school.mjc.stage0.loops.task3;
 
 public class AlphabetPrinter {
     public void printAlphabet() {
+        char x;
+
+        for(x = 'a'; x <= 'z'; x++){
+            System.out.println(x);
+        }
     }
 }
